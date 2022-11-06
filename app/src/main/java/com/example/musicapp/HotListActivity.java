2434b7f3537d5147d43b78c6dc657f576fb23232
Viet Truong <viettruong0825@gmail.com>
2022-11-06 18:25:49 +0700

    update view home, search, library, hotlist

---
+++ app/src/main/java/com/example/musicapp/HotListActivity.java
@@ -0,0 +1,14 @@
+package com.example.musicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HotListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hot_list);
+    }
+}
